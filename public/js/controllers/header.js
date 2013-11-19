baaab767f50499563c26f99e15fc14ee50950e9b
nickeblewis <nickeblewis@gmail.com>
2013-11-19 22:03:14 +0000

    Removed unwanted nav items

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -2,15 +2,6 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
     $scope.global = Global;
 
     $scope.menu = [{
-        "title": "Articles",
-        "link": "articles"
-    }, {
-        "title": "Create New Article",
-        "link": "articles/create"
-    }, {
-        "title": "Places",
-        "link": "places"
-    }, {
         "title": "Create new place",
         "link": "places/create"
     }];
